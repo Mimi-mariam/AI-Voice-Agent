8d51b052ffa75a9a56ea7b86269e533ae509d8ee
Mariam Abdulrasheed <folshadeolaitan00023@gmail.com>
2026-08-22 02:02:29 +0100

    store commit

--- src/app/api/tools/booking/create/route.ts
+++ src/app/api/tools/booking/create/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import prisma from "@/lib/database";
 import { createCalBooking } from "@/lib/cal";
 
-export default async function POST(req: Request) {
+export async function POST(req: Request) {
   let messageId = "unknown";
   try {
     const body = await req.json();
